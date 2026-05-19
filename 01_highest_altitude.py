ab6765bb0f5327203c9aaa1fd05a90a25cd3596d
Afia-Mobasshira2 <afiamobasshira2@gmail.com>
2026-05-19 10:26:57 +0600

    first commit

---
+++ 01_highest_altitude.py
@@ -0,0 +1,17 @@
+def largestAltitude(gain):
+    current_altitude = 0
+    max_altitude = 0
+    
+    # Write a loop to go through each tracking number in the 'gain' list
+    for g in gain:
+        # 1. Update your current_altitude by adding 'g' to it
+        
+        # 2. Check if the new current_altitude is greater than max_altitude.
+        #    If it is, update max_altitude!
+        
+        pass # Delete this 'pass' when you write your code
+        
+    return max_altitude
+
+# Test case to check your logic
+print(largestAltitude([-5, 1, 5, 0, -7])) # Should output 1
